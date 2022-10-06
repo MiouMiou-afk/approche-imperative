b5bb719ef200e25200d73120ad65db9086b1e4b2
Myriam <myriam@cinetwork.app>
2022-10-06 16:39:21 +0200

    add tp 03 - déclaration de variable

---
+++ src/fr/declaration/variable/DeclarationApp.java
@@ -0,0 +1,28 @@
+package fr.declaration.variable;
+
+public class DeclarationApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	byte b = 1;
+	short s = -1256;
+	int i = 256987463;
+	long l = 16568656L;
+	float f =2.5f;
+	double d = 6.4D;
+	char c = 'h';
+	boolean myBool = true;
+	String string = "yoyo";
+	
+	System.out.println(myBool);	
+	
+	String randomString= "Voici le résultat d’un calcul :\n 1+5=6";
+	
+    System.out.println(randomString);
+	
+	
+		
+	}
+
+}
